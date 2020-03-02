6f9798a0ed65568e2dc5d4efb5b8aea849acfc4b
mia <sansolmom@gmail.com>
2020-03-03 01:22:48 +0900

    fourth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
+const Person = ({img, name, occupation, children}) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <img src={url} alt="person img" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{occupation}</h4>
+        {children}
+      </div>
     </div>
   );
+};
+const PersonList = () => {
+  return (
+    <section className="person-list">
+      <Person img="34" name="john" occupation="developer" />
+      <Person img="22" name="bob" occupation="designer">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
+          accusamus?
+        </p>
+      </Person>
+      <Person img="56" name="david" occupation="the boss" />
+    </section>
+  );
+};
+
+class App extends Component {
+  render() {
+    return <PersonList />;
+  }
 }
 
 export default App;
