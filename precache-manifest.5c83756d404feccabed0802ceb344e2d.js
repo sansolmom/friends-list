b8cd26b2c587b91008067589de7701c66dcf854b
mia <sansolmom@gmail.com>
2020-03-03 01:03:58 +0900

    Updates

---
+++ precache-manifest.5c83756d404feccabed0802ceb344e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787efdede36f570210f6268b1f96d133",
+    "url": "/friends-list/index.html"
+  },
+  {
+    "revision": "45b02d75370fb5083cc8",
+    "url": "/friends-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1cbff97ed3bef5205688",
+    "url": "/friends-list/static/js/2.59bfa0c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/friends-list/static/js/2.59bfa0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b02d75370fb5083cc8",
+    "url": "/friends-list/static/js/main.11c3bd0f.chunk.js"
+  },
+  {
+    "revision": "0ace481571410ea43c12",
+    "url": "/friends-list/static/js/runtime-main.17dbe3b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/friends-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
